661588cf79b247d4adf55a87cc4094cd8bf451c2
EggsyJ <58285674+JeonChanMin@users.noreply.github.com>
2023-04-30 23:08:07 +0900

    modified backround and clock

--- js/background.js
+++ js/background.js
@@ -1,7 +1,8 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const backgroundImageElement = document.createElement("img");
 
-backgroundImageElement.src = `image/${chosenImage}`;
+// document.body.style.backgroundImage = `url('/image/${chosenImage}')`;
+// document.body.style.background =
+//   "linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8))";
 
-document.body.appendChild(backgroundImageElement);
+document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('/image/${chosenImage}')`;
